fac95abc03770e62c0ed3f37edac42e374a4f0d7
pavlo.zaloznyi <pavlo.zaloznyi@pm.bet>
2022-08-28 14:18:46 +0300

    utils for hw6 part 2

--- src/main/java/com/pzaloznyi/utils/UUIDService.java
+++ src/main/java/com/pzaloznyi/utils/UUIDService.java
@@ -1,2 +1,17 @@
-package com.pzaloznyi.utils;public class UUIDService {
+package com.pzaloznyi.utils;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Service
+public class UUIDService {
+    public List<UUID> generate(long size) {
+        return Stream.generate(UUID::randomUUID)
+                .limit(size)
+                .collect(Collectors.toList());
+    }
 }
